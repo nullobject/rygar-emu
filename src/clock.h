72b2dc73cbd800e4a2e1a79fa64f59c8d87666b6
Joshua Bassett <hello@joshbassett.info>
2022-04-05 19:01:15 +1000

    Update external files

--- src/clock.h
+++ src/clock.h
@@ -2,39 +2,42 @@
 /*
     Emulator frame timing helper functions.
 */
-extern void clock_init(void);
-extern uint32_t clock_frame_time(void);
-extern uint32_t clock_frame_count(void);
+void clock_init(void);
+uint32_t clock_frame_time(void);
+uint32_t clock_frame_count_60hz(void);
 
 /*== IMPLEMENTATION ==========================================================*/
 #ifdef COMMON_IMPL
-#include "sokol_time.h"
+#include "sokol_app.h"
+#include <assert.h>
 
 typedef struct {
-    uint32_t frame_count;
-    uint64_t last_time_stamp;
-} clock_state;
-static clock_state clck;
+    bool valid;
+    uint64_t cur_time;
+} clock_state_t;
+static clock_state_t clck;
 
 void clock_init(void) {
-    stm_setup();
-    clck.frame_count = 0;
-    clck.last_time_stamp = stm_now();
+    clck = (clock_state_t) {
+        .valid = true,
+        .cur_time = 0,
+    };
 }
 
 uint32_t clock_frame_time(void) {
-    clck.frame_count++;
-    uint32_t frame_time_us = (uint32_t) stm_us(stm_laptime(&clck.last_time_stamp));
-    if (frame_time_us < 24000) {   /* 24 ms */
-        frame_time_us = 16667;
+    assert(clck.valid);
+    uint32_t frame_time_us = (uint32_t) (sapp_frame_duration() * 1000000.0);
+    // prevent death-spiral on host systems that are too slow to emulate
+    // in real time, or during long frames (e.g. debugging)
+    if (frame_time_us > 24000) {
+        frame_time_us = 24000;
     }
-    else {
-        frame_time_us = 33333;
-    }
+    clck.cur_time += frame_time_us;
     return frame_time_us;
 }
 
-uint32_t clock_frame_count(void) {
-    return clck.frame_count;
+uint32_t clock_frame_count_60hz(void) {
+    assert(clck.valid);
+    return (uint32_t) (clck.cur_time / 16667);
 }
 #endif /* COMMON_IMPL */
